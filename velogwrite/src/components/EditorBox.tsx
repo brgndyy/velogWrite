24974a3e2d7478b231ae4f28d7295376efb85482
brgndy <brgndy96@gmail.com>
2023-04-18 21:11:17 +0900

    msg

--- velogwrite/src/components/EditorBox.tsx
+++ velogwrite/src/components/EditorBox.tsx
@@ -1,39 +1,15 @@
 import React from "react";
-import { Editor } from "@toast-ui/react-editor";
-import "tui-color-picker/dist/tui-color-picker.css";
-import "@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css";
+
 import classes from "./EditorBox.module.css";
-import "@toast-ui/editor/dist/toastui-editor.css";
-import "@toast-ui/editor/dist/theme/toastui-editor-dark.css";
-import colorSyntax from "@toast-ui/editor-plugin-color-syntax";
-import "tui-color-picker/dist/tui-color-picker.css";
+
 type ContentType = {
   content: string;
 };
 
-const toolbarItems = [
-  ["heading", "bold", "italic", "strike"],
-  ["hr"],
-  ["ul", "ol", "task"],
-  ["table", "link"],
-  ["image"],
-  ["code"],
-  ["scrollSync"],
-];
-
 export default function EditorBox({ content }: ContentType) {
   return (
     <>
-      <div className={classes.editorBox_container}>
-        <Editor
-          toolbarItems={toolbarItems}
-          initialValue={content}
-          placeholder="글을 작성해주세요"
-          plugins={[colorSyntax]}
-          height="60rem"
-          initialEditType="markdown"
-        />
-      </div>
+      <div className={classes.editorBox_container}></div>
     </>
   );
 }

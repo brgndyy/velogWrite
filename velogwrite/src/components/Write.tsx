0d1e679d2a832736f197a9d63c7cd384b9f92f8b
brgndy <brgndy96@gmail.com>
2023-04-18 16:38:40 +0900

    msg

--- velogwrite/src/components/Write.tsx
+++ velogwrite/src/components/Write.tsx
@@ -3,13 +3,17 @@
 import classes from "./Write.module.css";
 import Preview from "./Preview";
 import { useState } from "react";
+import dynamic from "next/dynamic";
+
+const EditorBox = dynamic(() => import("./EditorBox"), { ssr: false });
 
 export default function Write() {
-  const [content, setContent] = useState("");
+  const [content, setContent] = useState("안녕하세요");
 
   return (
     <>
       <div className={classes.write_card}>
+        <EditorBox content={content} />
         <Preview content={content} />
       </div>
     </>

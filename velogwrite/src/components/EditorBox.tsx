7e2cf6540c36948626d12ff567ccbe40c8676bc5
brgndy <brgndy96@gmail.com>
2023-04-18 22:31:40 +0900

    msg

--- velogwrite/src/components/EditorBox.tsx
+++ velogwrite/src/components/EditorBox.tsx
@@ -1,15 +1,54 @@
-import React from "react";
-
+import ReactCodeMirror from "@uiw/react-codemirror";
+import {
+  loadLanguage,
+  langNames,
+  langs,
+} from "@uiw/codemirror-extensions-langs";
 import classes from "./EditorBox.module.css";
+import createTheme from "@uiw/codemirror-themes";
 
 type ContentType = {
   content: string;
+  contentHandler: (e) => void;
 };
 
-export default function EditorBox({ content }: ContentType) {
+const myTheme = createTheme({
+  theme: "dark",
+  settings: {
+    background: "#121212",
+    foreground: "#4D4D4C",
+    caret: "#AEAFAD",
+    selection: "#D6D6D6",
+    selectionMatch: "#D6D6D6",
+    gutterBackground: "#FFFFFF",
+    gutterForeground: "#4D4D4C",
+    gutterBorder: "#ddd",
+    gutterActiveForeground: "",
+    lineHighlight: "#EFEFEF",
+  },
+  styles: [],
+});
+
+loadLanguage("tsx");
+
+export default function EditorBox({ content, contentHandler }: ContentType) {
   return (
     <>
-      <div className={classes.editorBox_container}></div>
+      <div className={classes.editorBox_container}>
+        <ReactCodeMirror
+          theme={myTheme}
+          value={content}
+          onChange={contentHandler}
+          height="200px"
+          basicSetup={{
+            foldGutter: false,
+            lineNumbers: false,
+            indentOnInput: false,
+            highlightActiveLine: false,
+          }}
+          extensions={[langs.tsx()]}
+        />
+      </div>
     </>
   );
 }
--- velogwrite/src/components/Write.tsx
+++ velogwrite/src/components/Write.tsx
@@ -3,17 +3,20 @@
 import classes from "./Write.module.css";
 import Preview from "./Preview";
 import { useState } from "react";
-import dynamic from "next/dynamic";
-
-const EditorBox = dynamic(() => import("./EditorBox"), { ssr: false });
+import EditorBox from "./EditorBox";
 
 export default function Write() {
   const [content, setContent] = useState("안녕하세요");
 
+  const contentHandler = (e: any) => {
+    setContent(e.current.innerHTML);
+    console.log(content);
+  };
+
   return (
     <>
       <div className={classes.write_card}>
-        <EditorBox content={content} />
+        <EditorBox content={content} contentHandler={contentHandler} />
         <Preview content={content} />
       </div>
     </>

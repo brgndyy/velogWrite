412adac7fca7be9c12bc401a5bd2eafa10b08df0
brgndy <brgndy96@gmail.com>
2023-04-20 16:22:29 +0900

    msg

--- velogwrite/src/components/EditorBox.tsx
+++ velogwrite/src/components/EditorBox.tsx
@@ -1,68 +1,65 @@
 import ReactCodeMirror from "@uiw/react-codemirror";
-import {
-  loadLanguage,
-  langNames,
-  langs,
-} from "@uiw/codemirror-extensions-langs";
 import classes from "./EditorBox.module.css";
 import createTheme from "@uiw/codemirror-themes";
 import { markdown, markdownLanguage } from "@codemirror/lang-markdown";
 import { languages } from "@codemirror/language-data";
-
-const code = `## Title
-
-\`\`\`jsx
-function Demo() {
-  return <div>demo</div>
-}
-\`\`\`
-
-\`\`\`bash
-# Not dependent on uiw.
-npm install @codemirror/lang-markdown --save
-npm install @codemirror/language-data --save
-\`\`\`
-
-[weisit ulr](https://uiwjs.github.io/react-codemirror/)
-
-\`\`\`go
-package main
-import "fmt"
-func main() {
-  fmt.Println("Hello, 世界")
-}
-\`\`\`
-`;
+import { EditorView } from "@codemirror/view";
+import { javascript } from "@codemirror/lang-javascript";
+import { tags as t } from "@lezer/highlight";
 
 type ContentType = {
   content: string;
   contentHandler: () => void;
 };
 
+const markdownStyles = [
+  { tag: t.heading1, fontWeight: "bold", fontSize: "2.5rem" },
+  { tag: t.heading2, fontWeight: "bold", fontSize: "2rem" },
+  { tag: t.heading3, fontWeight: "bold", fontSize: "1.17em" },
+  { tag: t.heading4, fontWeight: "bold", fontSize: "1em" },
+  { tag: t.heading5, fontWeight: "bold", fontSize: ".83em" },
+  { tag: t.heading6, fontWeight: "bold", fontSize: ".67em" },
+  { tag: t.strong, fontWeight: "bold" },
+];
+
 const myTheme = createTheme({
   theme: "dark",
   settings: {
-    background: "#121212",
-    foreground: "#4D4D4C",
-    caret: "#AEAFAD",
-    selection: "#D6D6D6",
-    selectionMatch: "#D6D6D6",
-    gutterBackground: "#FFFFFF",
-    gutterForeground: "#4D4D4C",
-    gutterBorder: "#ddd",
-    gutterActiveForeground: "",
-    lineHighlight: "#EFEFEF",
+    background: "#171717",
+    foreground: "#ffffff",
+    caret: "#ffffff",
+    selection: "#036dd626",
+    selectionMatch: "#036dd626",
+    lineHighlight: "#8a91991a",
+    gutterBackground: "#fff",
+    gutterForeground: "#8a919966",
   },
-  styles: [],
+  styles: [
+    { tag: t.comment, color: "#787b8099" },
+    { tag: t.variableName, color: "#0080ff" },
+    { tag: [t.string, t.special(t.brace)], color: "#5c6166" },
+    { tag: t.number, color: "#5c6166" },
+    { tag: t.bool, color: "#5c6166" },
+    { tag: t.null, color: "#5c6166" },
+    { tag: t.keyword, color: "#5c6166" },
+    { tag: t.operator, color: "#5c6166" },
+    { tag: t.className, color: "#5c6166" },
+    { tag: t.definition(t.typeName), color: "#5c6166" },
+    { tag: t.typeName, color: "#5c6166" },
+    { tag: t.angleBracket, color: "#5c6166" },
+    { tag: t.tagName, color: "#5c6166" },
+    { tag: t.attributeName, color: "#5c6166" },
+    ...markdownStyles,
+  ],
 });
-
-loadLanguage("tsx");
 
 export default function EditorBox({ content, contentHandler }: ContentType) {
   return (
     <>
       <div className={classes.editorBox_container}>
         <ReactCodeMirror
+          className={classes.code_mirror}
+          theme={myTheme}
           value={content}
           onChange={contentHandler}
           height="100vh"
@@ -73,6 +70,8 @@ export default function EditorBox({ content, contentHandler }: ContentType) {
           }}
           extensions={[
             markdown({ base: markdownLanguage, codeLanguages: languages }),
+            EditorView.lineWrapping,
+            javascript({ jsx: true }),
           ]}
         />
       </div>
--- velogwrite/src/components/Preview.tsx
+++ velogwrite/src/components/Preview.tsx
@@ -1,6 +1,17 @@
 import classes from "./Preview.module.css";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import remarkGfm from "remark-gfm";
+import Image from "next/image";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { materialDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+
+const value = `hello
+\n
+\n
+world
+`;
+
+const source = value.replace(/\n/gi, "\n &nbsp;");
 
 type ContentType = {
   content: string;
@@ -10,7 +21,38 @@ export default function Preview({ content }: ContentType) {
   return (
     <>
       <div className={classes.preview_container}>
-        <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+        <ReactMarkdown
+          className={classes.markdown}
+          remarkPlugins={[remarkGfm]} // Allows us to have embedded HTML tags in our markdown
+          components={{
+            code({ node, inline, className, children, ...props }) {
+              const match = /language-(\w+)/.exec(className || "");
+              return !inline && match ? (
+                <SyntaxHighlighter
+                  language={match[1]}
+                  PreTag="div"
+                  {...props}
+                  style={materialDark}
+                >
+                  {String(children).replace(/\n$/, "")}
+                </SyntaxHighlighter>
+              ) : (
+                <code {...props}>{children}</code>
+              );
+            },
+            img: (image) => (
+              <Image
+                src={image.src || ""}
+                alt={image.alt || ""}
+                width={500}
+                height={300}
+                className={classes.markdown_container_img}
+              />
+            ),
+          }}
+        >
+          {content}
+        </ReactMarkdown>
       </div>
     </>
   );
